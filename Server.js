5630d231a70a703b62d54f0d013992157d188ca3
pradeep007p <pradeepkori007pk@gmail.com>
2025-09-26 10:10:30 +0545

    Commit directly to main branch

---
+++ Server.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => res.send("MegaMall Million Backend Running"));
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("MongoDB connected");
+    app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`));
+  })
+  .catch(err => console.log("MongoDB connection error:", err));
+      
